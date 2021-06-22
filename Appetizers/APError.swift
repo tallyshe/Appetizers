0410ecaf6b3f8b36d5691e68adbd3b1fd3ac927b
tallyshe <karhoshe@gmail.com>
2021-06-23 02:06:43 +0800

    NetworkManager 1. Add Network manager for network call 2. Conform to Decodable for AppetizerResponse in data 3. add Enum for Error for network call response

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Kar Ho She on 23/6/2021.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
